5e680942b90e788d8a7a327ddb2fdc31c9965dde
habiba zguaid <zguaidhabiba@gmail.com>
2024-10-15 21:27:19 +0100

    Adding cors to the app.js

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const appointmentRouter = require('./routes/appointmentRoute');
 const doctorsRouter = require('./routes/doctorsRoute');
 const adminRouter = require('./routes/adminRoute');
 const patientRouter = require('./routes/patientRoute');
+const nursesRouter = require('./routes/nursesRoute');
 
 const HOSTNAME = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const app = express();
 
+app.use('/pics', express.static(path.join(__dirname, 'pics')));
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/admin', adminRouter);
 app.use('/api/appointments', appointmentRouter);
