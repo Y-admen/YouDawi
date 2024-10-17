3aa4440530836e7d5fa6d25b143134d2ec82e72c
Y-admen <yassminash305@gmail.com>
2024-10-17 16:08:58 +0300

    Handled Schaduling Appointment Email

--- utils/schedulingNotifications.js
+++ utils/schedulingNotifications.js
@@ -5,17 +5,16 @@ const Patient = require('../models/patientModel');
 const sendEmail = require('./mailUtils');
 
 
-cron.schedule('* * * * *', async() => {
-    console.log("Cron job triggered");
+cron.schedule('0 0 * * *', async() => {
     try {
         const oneDayAhead = new Date();
         oneDayAhead.setDate(oneDayAhead.getDate() + 1);
 
+        // get all coming appointments between 24 to 48 hour from now 
         const appointments = await Appointment.find({
             appointmentDate: { $gte: oneDayAhead, $lt: new Date(oneDayAhead.getTime() + 24 * 60 * 60 * 1000) },
             status: 'Pending',
         });
-        console.log(`Found ${appointments.length} appointments`);
 
         appointments.forEach(async(appointment) => {
             try {
@@ -39,5 +38,4 @@ cron.schedule('* * * * *', async() => {
     } catch (err) {
         console.error("Error in cron job:", err);
     }
-    console.log("appointments checked from schedule");
 });
