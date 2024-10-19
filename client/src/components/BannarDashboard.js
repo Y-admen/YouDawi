27f9365515007a81af7a4acfe4b56f1bf8edbcd8
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-10-20 00:53:21 +0300

    Added a new component

---
+++ client/src/components/BannarDashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BannarDashoard() {
+  return (
+    <div>
+      <img src="../pics/bannarDashboard.png" />
+      <div>
+        <h1>Lokking for specailist doctor?</h1>
+        <p>Uplaod a Prescription and Tell Us what you need. We do the Rest</p>
+        <button>BOOK NOW</button>
+      </div>
+    </div>
+  );
+}
+
+export default BannarDashoard;
