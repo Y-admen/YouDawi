1c3d95381ced06d3ee84d7dee9be601283d6bcf0
habiba zguaid <zguaidhabiba@gmail.com>
2024-10-23 17:35:54 +0100

    Updated the App.js with prescription imports

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const doctorsRouter = require('./routes/doctorsRoute');
 const adminRouter = require('./routes/adminRoute');
 const patientRouter = require('./routes/patientRoute');
 const nursesRouter = require('./routes/nursesRoute');
+const prescriptionRouter = require('./routes/prescriptionRoute');
 const httpStatusText = require('./utils/httpStatusText');
 
 const HOSTNAME = '127.0.0.1';
@@ -27,6 +28,7 @@ app.use('/api/appointments', appointmentRouter);
 app.use('/api/doctors', doctorsRouter);
 app.use('/api/nurses', nursesRouter);
 app.use('/api/patients', patientRouter);
+app.use('/api/prescriptions', prescriptionRouter);
 
 app.use((error, req, res, next) => {
     res.status(error.statusCode || 500).json({ status: error.statusText || httpStatusText.ERROR, message: error.message, code: error.statusCode || 500, data: null });
