4cbd2fb2ff587af81c266865516608c9f6038e81
489Esraa <asraaalaa2050@gmail.com>
2024-10-23 12:23:23 +0300

    Add footer to the landingpage , Make route in App.js

--- client/src/components/Footer_H.js
+++ client/src/components/Footer_H.js
@@ -2,7 +2,7 @@ import { faFacebookF, faInstagram, faLinkedin, faTelegram, faTwitter } from '@fo
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-function Footer_h() {
+function Footer_H() {
     return (
     <footer className="bg-blue-800 text-white py-4">
             <div className="container mx-auto flex justify-between">
@@ -11,10 +11,10 @@ function Footer_h() {
                 <h2 className="text-2xl font-bold"></h2>
                 </div>
                 
-           <h1 className="text-6xl">You<span className="text-yellow-500">Dawi</span></h1>
+             <h1 className="text-4xl">YouDawi</h1>
             <div className="flex space-x-4">
                 
-                <a href="#" className="text-zinc-200 font-serif text-4xl">Are You A Doctor?</a>
+                <a href="#" className="text-zinc-200 font-serif text-lg">Are You A Doctor?</a>
                                
                 </div>
                 <pr />
@@ -48,4 +48,4 @@ function Footer_h() {
     );
 }
 
-export default Footer_h;
+export default Footer_H;
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from './Navbar';
 // import myImage from '../pics/photo_doctors.jpg';
 // import myImage2 from '../pics/home_visit.jpeg';
 // import myImage3 from '../pics/mobile.jpeg';
 import './style.css'
-import FooterH from './Footer';
+import Footer_H from './Footer_H';
 import Banner from './Banner';
 import About from './About';
 import Reviews from "./Reviews";
 import Info from './Info';
 
 function LandingPage() {
+  // useEffect(() => {
+  //   // Dynamically import Bootstrap CSS and JS only for this page
+  //   import('bootstrap/dist/css/bootstrap.min.css')
+  //     .then(() => {
+  //       return import('bootstrap/dist/js/bootstrap.bundle.min');
+  //     })
+  //     .catch((error) => console.error('Error loading Bootstrap', error));
+  // }, []); // Empty dependency array ensures this runs only once on component mount
+
   return (
     <div>
-      <Navbar />
-      <Banner/>
+          <Navbar />
+           <Banner/>
       <Info />
       <About />
       <Reviews />
-      <FooterH />
+      <Footer_H />
     </div>
   );
 }
