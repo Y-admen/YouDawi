b20d4bc140f683bbb54c8947cde4123afd2d6670
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-10-25 14:52:16 +0300

    update generateJWT file in utils

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (payload) => {
-
+    // Generate a JWT token
     const token = await jwt.sign(
         payload,
         process.env.JWT_SECRET_KEY,
