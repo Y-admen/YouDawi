53489186361e4a69836089c564d132020857cd73
489Esraa <asraaalaa2050@gmail.com>
2024-10-25 15:36:43 +0300

    Update Landingpage

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import VerticalNavbar from './VerticalNavbar';
 import Modal from './Modal';
-import { useState, useEffect } from 'react';
-import ProfileSettings from './ProfileSettings';
+import { useState } from 'react';
+import ProfileSettings from './ProfileSettingspat';
+import DoctorNurseDashboard from './DoctorNurseDashboard';
+import PatientDashboard from './PatientDashboard';
 
-// Assuming role is either passed via props or fetched from localStorage/context
 function Layout({ role }) {
-    // State for controlling modal visibility
     const [isModalVisible, setModalVisible] = useState(false);
+
 
     // Function to open the modal
     const openModal = () => {
@@ -19,31 +20,32 @@ function Layout({ role }) {
         setModalVisible(false);
     };
 
-    // Logic to render the dashboard based on role
+    // Function to render dashboard based on the user's role
     const renderDashboard = () => {
         switch (role) {
             case 'doctor':
-                return <DoctorDashboard />;
+                return <DoctorNurseDashboard role={role} />;
             case 'patient':
                 return <PatientDashboard />;
             case 'nurse':
-                return <NurseDashboard />;
+                return <DoctorNurseDashboard role={role} />;
             default:
                 return <DefaultDashboard />;
         }
     };
 
     return (
-      <div className="flex h-screen bg-primary-30">
-                {/* Pass openModal function to VerticalNavbar */}
-                <VerticalNavbar onOpenModal={openModal} />
+        <div className="flex h-screen bg-gradient-to-r from-primary-10 to-primary-35 pt-1 pb-1">
+            {/* Pass openModal function to VerticalNavbar */}
+            <VerticalNavbar onOpenModal={openModal} userRole={role} />
 
-                <div className="flex-1 overflow-auto border-7 rounded-3xl border-primary-30  bg-gray-100 p-6">
-                    {/* Render the appropriate dashboard based on the user's role */}
-                    {renderDashboard()}
-                    <Outlet />
-                </div>
+            {/* <div className="w-2 min-w-2"></div> */}
 
+            <div className="flex-1 overflow-auto border-7 rounded-3xl border-primary-30  bg-gray-100 p-6">
+                {/* Render the appropriate dashboard based on the user's role */}
+                {/* {renderDashboard()} */}
+                <Outlet />
+            </div>
 
             {/* Profile Component - Triggered within layout */}
             {isModalVisible && (
@@ -58,18 +60,6 @@ function Layout({ role }) {
     );
 }
 
-// Dummy components for each role's dashboard
-function DoctorDashboard() {
-    return <div>Doctor Dashboard</div>;
-}
-
-function PatientDashboard() {
-    return <div>Patient Dashboard</div>;
-}
-
-function NurseDashboard() {
-    return <div>Nurse Dashboard</div>;
-}
 
 function DefaultDashboard() {
     return <div>Default Dashboard</div>;
