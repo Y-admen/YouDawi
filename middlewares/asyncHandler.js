fb2c30aac704a0108263db27242f1dd157bf417d
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-10-25 15:05:48 +0300

    'express-async-handler' package

--- middlewares/asyncHandler.js
+++ middlewares/asyncHandler.js
@@ -1,7 +1,8 @@
+//we can replace it later with the 'express-async-handler' package
 module.exports = (asyncFn) => {
     return (req, res, next) => {
         asyncFn(req, res, next).catch((err) => {
             next(err);
         });
     }
-} //we can replace it later with the 'express-async-handler' package
+} 
